8d012214b2a38978ea8460f6294edc94838abad0
Amitesh Agrawal <amiteshrocks02@gmail.com>
2020-09-03 19:26:52 +0530

    firstcommit

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+console.log("server is running")
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(3000); 
